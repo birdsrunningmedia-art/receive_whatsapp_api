edd8feb12fa0bb592a3e928356366e6d12c6f947
Yekini Abdulwahab <birdsrunningmedia@gmail.com>
2026-08-13 14:57:41 +0100

    added the webhook

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 import express from "express";
+import axios from "axios";
+import { configDotenv } from "dotenv";
+
+// Load environment variables from .env file
+configDotenv();
 
 const app = express();
 const PORT = 3000;
+const token = process.env.WEBHOOK_VERIFY_TOKEN;
 
 app.get("/", (req, res) => {
   res.send("Server is live with ES modules!");
+});
+
+app.get("/webhook", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const challenge = req.query["hub.challenge"];
+  const token = req.query["hub.verify_token"];
 });
 
 app.listen(PORT, () => {
